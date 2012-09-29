2550fa5ce977c83e06d77cbbade8adf66059433c
peter popov <ironspeter@gmail.com>
2012-09-29 22:33:34 +0400

    works under linux; no need in -lpthread on macos

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 echo "build bench with block alloc"
-g++ -O2 main.cpp alloc.cpp -o bench_blk
+g++ -O2 main.cpp alloc.cpp -lpthread -o bench_blk
 echo "done. run bench"
 time ./bench_blk
 echo "build bench with std alloc"
-g++ -O2 main.cpp -o bench_std
+g++ -O2 main.cpp -lpthread -o bench_std
 echo "done. run bench"
 time ./bench_std
 
